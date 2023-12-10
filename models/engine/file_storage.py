6eb4f5621631b493afd5e641f1809cfd6fd8e185
omar546 <omer.mkawy.om@gmail.com>
2023-12-10 02:47:46 +0200

    f

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,61 +30,60 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
-    def classes(self):
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
+    # def classes(self):
+    #     from models.base_model import BaseModel
+    #     from models.user import User
+    #     from models.state import State
+    #     from models.city import City
+    #     from models.amenity import Amenity
+    #     from models.place import Place
+    #     from models.review import Review
 
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
+    #     classes = {"BaseModel": BaseModel,
+    #                "User": User,
+    #                "State": State,
+    #                "City": City,
+    #                "Amenity": Amenity,
+    #                "Place": Place,
+    #                "Review": Review}
+    #     return classes
     
-    def attributes(self):
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"first_name": str,
-                      "last_name": str,
-                      "email": str,
-                      "password": str
-                      },
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                "user_id": str,
-                "text": str}
-        }
-        return attributes
+    # def attributes(self):
+    #     attributes = {
+    #         "BaseModel":
+    #                  {"id": str,
+    #                   "created_at": datetime.datetime,
+    #                   "updated_at": datetime.datetime},
+    #         "User":
+    #                  {"first_name": str,
+    #                   "last_name": str,
+    #                   "email": str,
+    #                   "password": str
+    #                   },
+    #         "State":
+    #                  {"name": str},
+    #         "City":
+    #                  {"state_id": str,
+    #                   "name": str},
+    #         "Amenity":
+    #                  {"name": str},
+    #         "Place":
+    #                  {"city_id": str,
+    #                   "user_id": str,
+    #                   "name": str,
+    #                   "description": str,
+    #                   "price_by_night": int,
+    #                   "latitude": float,
+    #                   "longitude": float,
+    #                   "number_rooms": int,
+    #                   "number_bathrooms": int,
+    #                   "max_guest": int,
+    #                   "amenity_ids": list},
+    #         "Review":
+    #         {"place_id": str,
+    #             "user_id": str,
+    #             "text": str}
+    #     }
+    #     return attributes
