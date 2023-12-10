435b6fba62e7479a017d3bf25ac8ae015dcd5064
omar546 <omer.mkawy.om@gmail.com>
2023-12-10 19:22:06 +0200

    بب

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,58 +37,59 @@ class FileStorage:
                         for k, v in obj_dict.items()}
             FileStorage.__objects = obj_dict
 
-    # def classes(self):
-    #     from models.base_model import BaseModel
-    #     from models.user import User
-    #     from models.state import State
-    #     from models.city import City
-    #     from models.amenity import Amenity
-    #     from models.place import Place
-    #     from models.review import Review
+    def classes(self):
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
-    #     classes = {"BaseModel": BaseModel,
-    #                "User": User,
-    #                "State": State,
-    #                "City": City,
-    #                "Amenity": Amenity,
-    #                "Place": Place,
-    #                "Review": Review}
-    #     return classes
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
     
-    # def attributes(self):
-    #     attributes = {
-    #         "BaseModel":
-    #                  {"id": str,
-    #                   "created_at": datetime.datetime,
-    #                   "updated_at": datetime.datetime},
-    #         "User":
-    #                  {"first_name": str,
-    #                   "last_name": str,
-    #                   "email": str,
-    #                   "password": str
-    #                   },
-    #         "State":
-    #                  {"name": str},
-    #         "City":
-    #                  {"state_id": str,
-    #                   "name": str},
-    #         "Amenity":
-    #                  {"name": str},
-    #         "Place":
-    #                  {"city_id": str,
-    #                   "user_id": str,
-    #                   "name": str,
-    #                   "description": str,
-    #                   "price_by_night": int,
-    #                   "latitude": float,
-    #                   "longitude": float,
-    #                   "number_rooms": int,
-    #                   "number_bathrooms": int,
-    #                   "max_guest": int,
-    #                   "amenity_ids": list},
-    #         "Review":
-    #         {"place_id": str,
-    #             "user_id": str,
-    #             "text": str}
-    #     }
-    #     return attributes
+    def attributes(self):
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"first_name": str,
+                      "last_name": str,
+                      "email": str,
+                      "password": str
+                      },
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                "user_id": str,
+                "text": str}
+        }
+        return attributes
+    
