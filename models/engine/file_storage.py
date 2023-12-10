5f9639ea2b97d5a06a6a9284870f119dc95072ee
SeragAmged <seragamged2002@gmail.com>
2023-12-10 19:27:11 +0200

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,20 +24,11 @@ class FileStorage:
     def save(self):
         """save to JSON"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as fi:
-            di = {ky: val.to_dict() for ky, val in FileStorage.__objects.items()}
+            di = {ky: val.to_dict()
+                  for ky, val in FileStorage.__objects.items()}
             json.dump(di, fi)
 
     def reload(self):
-        """reload from JSON"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            FileStorage.__objects = obj_dict
-
-    def classes(self):
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
@@ -45,51 +36,19 @@ class FileStorage:
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-
+        """reload from JSON"""
         classes = {"BaseModel": BaseModel,
                    "User": User,
                    "State": State,
                    "City": City,
                    "Amenity": Amenity,
                    "Place": Place,
-                   "Review": Review}
-        return classes
-    
-    def attributes(self):
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"first_name": str,
-                      "last_name": str,
-                      "email": str,
-                      "password": str
-                      },
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                "user_id": str,
-                "text": str}
-        }
-        return attributes
-    
+                   "Review": Review, }
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+                obj_dict = {k: classes[v["__class__"]](**v)
+                            for k, v in obj_dict.items()}
+                FileStorage.__objects = obj_dict
+        except FileNotFoundError:
+            pass
