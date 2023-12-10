236c9ef9b05264a3657b557fb2d1e2d8951f3d40
SeragAmged <seragamged2002@gmail.com>
2023-12-10 20:24:23 +0200

    consle 1

---
+++ console.py
@@ -0,0 +1,19 @@
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+
+    def do_quit(self, comand):
+        """Quit command to exit the program"""
+        exit()
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        exit()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
