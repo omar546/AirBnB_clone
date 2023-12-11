1a70591d1286ad00c3407526fa3f5b9abc161e59
SeragAmged <seragamged2002@gmail.com>
2023-12-11 15:40:34 +0200

    33

--- console.py
+++ console.py
@@ -34,7 +34,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, comand):
         """Quit command to exit the program"""
-        exit()
+        return True
 
     def help_quit(self):
         """ Prints the help documentation for quit"""
@@ -42,8 +42,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """ Handles EOF to exit program """
-        print()
-        exit()
+        return True
 
     def help_EOF(self):
         """ Prints the help documentation for EOF  """
